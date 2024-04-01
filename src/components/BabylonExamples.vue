5cd74d6be3cd040c4d7ef08e404e777542e817b9
Taein Jang <tein@kakao.com>
2024-04-01 21:37:23 +0900

    first

---
+++ src/components/BabylonExamples.vue
@@ -0,0 +1,46 @@
+<template>
+
+<div>
+  <h3>Babylone Examples</h3>
+  <canvas></canvas>
+</div>
+<button>Click me!</button>
+
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+// import {BasicScene} from '@/BabylonExamples/BasicScene'
+import {StandardMaterials} from "@/BabylonExamples/StandardMaterials"
+export default defineComponent({
+  name: 'BabylonExamples',
+  mounted(){
+    const canvas = document.querySelector("canvas")!;
+    // new BasicScene(canvas);
+    new StandardMaterials(canvas)
+  }
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+canvas{
+  width:70%;
+  height:70%;
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
